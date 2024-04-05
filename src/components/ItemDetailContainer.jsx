d0303cc2d1ea6c81da2927adbb2b5d942a109f4a
Florencia Simon <florencia_simon@hotmail.com>
2024-04-04 21:43:45 -0300

    agregando itemCount

--- src/components/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 
 import Container from 'react-bootstrap/Container';
 import { useParams } from "react-router-dom";
+import { ItemCount } from './ItemCount';
 
 import data from "../data/products.json";
 
@@ -23,10 +24,13 @@ export const ItemDetailContainer = () => {
 
     if(!item) return <div>Cargando...</div>;
 
-    return <Container>
-        <img src={item.pictureUrl} style={{ width: '60vh'}}/>
-        <h1 style={{ color: 'whitesmoke', fontSize: '24px'}}>{item.title}</h1>
-        <h2 style={{ color: 'whitesmoke', fontSize: '20px'}}>{item.description}</h2>
-        <h3 style={{ color: 'whitesmoke', fontSize: '20px'}}>{item.price}</h3>
-    </Container>
+    return <div className='DetalleProducto'>
+        <Container className='ContainerProducto'>
+            <img src={item.pictureUrl} style={{ width: '60vh'}}/>
+            <h1 style={{ color: 'whitesmoke', fontSize: '24px'}}>{item.title}</h1>
+            <h2 style={{ color: 'whitesmoke', fontSize: '20px'}}>{item.description}</h2>
+            <h3 style={{ color: 'whitesmoke', fontSize: '20px'}}>{item.price}</h3>
+        </Container>
+        <ItemCount initial={1} stock={10} onAdd={(quantity) => console.log('Cantidad agregada ',quantity)}/>
+    </div>
 };
